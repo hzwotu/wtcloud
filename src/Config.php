0299761ab65fa8ff39bfa769350697dbab278379
赵敬火 <zhaojinghuo@hzwotu.com>
2022-12-19 11:08:08 +0800

    权限校验

--- src/Config.php
+++ src/Config.php
@@ -4,12 +4,13 @@ namespace Wotu;
 class Config{
     private $serviceDomain = [
 //        'auth' => 'http://localhost:36006',
-        'auth' => 'http://114.55.62.34:36006',
-        'finance' => 'http://114.55.62.34:36015',
-        'admin' => 'http://114.55.62.34:36021',
-        'id' => 'http://114.55.62.34:39001',
-        'common-data' => 'http://114.55.62.34:36026',
-        'gateway' => 'https://api.cloud.wozhipei.com'
+        'auth' => 'http://47.98.193.2:36006',
+        'finance' => 'http://47.98.193.2:36015',
+        'admin' => 'http://47.98.193.2:36021',
+        'id' => 'http://47.98.193.2:39001',
+        'common-data' => 'http://47.98.193.2:36026',
+        'gateway' => 'https://api.cloud.wozp.cn'
+
     ];
     private $domainUrl;
     public function __construct($domain){
