611056b944d54ea2a7256edea215700d81427784
Tinywan <756684177@qq.com>
2022-12-27 18:45:02 +0800

    feat:推荐位管理配置文件为测试环境

--- src/Config.php
+++ src/Config.php
@@ -1,24 +1,44 @@
 <?php
 
 namespace Wotu;
+use ErrorException;
+
 class Config{
-    private $serviceDomain = [
-//        'auth' => 'http://localhost:36006',
-        'form' => 'http://114.55.62.34:36031',
-        'auth' => 'http://114.55.62.34:36006',
-        'finance' => 'http://114.55.62.34:36015',
-        'admin' => 'http://114.55.62.34:36021',
-        'id' => 'http://114.55.62.34:39001',
-        'common-data' => 'http://114.55.62.34:36026',
-        'gateway' => 'https://api.cloud.wozhipei.com'
+//    private $serviceDomain = [
+////        'auth' => 'http://localhost:36006',
+//        'form' => 'http://114.55.62.34:36031',
+//        'auth' => 'http://114.55.62.34:36006',
+//        'finance' => 'http://114.55.62.34:36015',
+//        'admin' => 'http://114.55.62.34:36021',
+//        'id' => 'http://114.55.62.34:39001',
+//        'common-data' => 'http://114.55.62.34:36026',
+//        'gateway' => 'https://api.cloud.wozhipei.com'
+//    ];
+
+    // 测试
+    private array $serviceDomain = [
+        'form' => 'http://47.98.193.2:36031',
+        'auth' => 'http://47.98.193.2:36006',
+        'finance' => 'http://47.98.193.2:36015',
+        'admin' => 'http://47.98.193.2:36021',
+        'id' => 'http://47.98.193.2:39001',
+        'common-data' => 'http://47.98.193.2:36026',
+        'gateway' => 'https://api.cloud.wozp.cn'
+
     ];
+
     private $domainUrl;
+
+    /**
+     * @throws ErrorException
+     */
     public function __construct($domain){
         if(empty($this->serviceDomain[$domain])){
-            throw new \ErrorException('服务不存在');
+            throw new ErrorException('服务不存在');
         }
         $this->domainUrl = $this->serviceDomain[$domain];
     }
+
     public function getServiceDomain(){
         return $this->domainUrl;
     }
