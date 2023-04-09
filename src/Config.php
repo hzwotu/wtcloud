24788ab3292683f88adeb43a84fa019bae87eb1a
万少波 <756684177@qq.com>
2023-04-09 15:01:05 +0800

    feat:配置文件替换为正式环境的

--- src/Config.php
+++ src/Config.php
@@ -4,26 +4,16 @@ namespace Wotu;
 use ErrorException;
 
 class Config{
-//    private $serviceDomain = [
-//        'form' => 'http://192.168.2.90:36031',
-//        'auth' => 'http://192.168.2.90:36006',
-//        'finance' => 'http://192.168.2.90:36015',
-//        'admin' => 'http://192.168.2.90:36021',
-//        'id' => 'http://192.168.2.90:39001',
-//        'common-data' => 'http://192.168.2.90:36026',
-//        'question' => 'http://192.168.2.90:36076',
-//        'sign-in' => 'http://192.168.2.90:36081',
-//        'gateway' => 'https://api.cloud.wozhipei.com',
-//    ];
-
-    private array $serviceDomain = [
-        'form' => 'http://47.98.193.2:36006',
-        'auth' => 'http://47.98.193.2:36006',
-        'finance' => 'http://47.98.193.2:36015',
-        'admin' => 'http://47.98.193.2:36021',
-        'id' => 'http://47.98.193.2:39001',
-        'common-data' => 'http://47.98.193.2:36026',
-        'gateway' => 'https://api.cloud.wozp.cn'
+    private $serviceDomain = [
+        'form' => 'http://192.168.2.90:36031',
+        'auth' => 'http://192.168.2.90:36006',
+        'finance' => 'http://192.168.2.90:36015',
+        'admin' => 'http://192.168.2.90:36021',
+        'id' => 'http://192.168.2.90:39001',
+        'common-data' => 'http://192.168.2.90:36026',
+        'question' => 'http://192.168.2.90:36076',
+        'sign-in' => 'http://192.168.2.90:36081',
+        'gateway' => 'https://api.cloud.wozhipei.com',
     ];
 
     private $domainUrl;
