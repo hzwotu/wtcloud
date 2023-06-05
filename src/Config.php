cdea1127402e09173f16311833d1ad97cd5e424a
miao933 <miao933@126.com>
2023-06-05 15:23:43 +0800

    test

--- src/Config.php
+++ src/Config.php
@@ -6,18 +6,18 @@ use ErrorException;
 class Config{
 
     private $serviceDomain = [
-        'tool' => 'http://192.168.2.90:36126',
-        'form' => 'http://192.168.2.90:36031',
-        'auth' => 'http://192.168.2.90:36006',
-        'finance' => 'http://192.168.2.90:36015',
-        'admin' => 'http://192.168.2.90:36021',
-        'id' => 'http://192.168.2.90:39001',
-        'common-data' => 'http://192.168.2.90:36026',
-        'question' => 'http://192.168.2.90:36076',
-        'sign-in' => 'http://192.168.2.90:36081',
-        'job' => 'http://192.168.2.90:36011',
-        'questionnaireBase' => 'http://192.168.2.90:36121',
-        'organization' => 'http://192.168.2.90:36106',
+        'tool' => 'http://47.98.193.2:36126',
+        'form' => 'http://47.98.193.2:36031',
+        'auth' => 'http://47.98.193.2:36006',
+        'finance' => 'http://47.98.193.2:36015',
+        'admin' => 'http://47.98.193.2:36021',
+        'id' => 'http://47.98.193.2:39001',
+        'common-data' => 'http://47.98.193.2:36026',
+        'question' => 'http://47.98.193.2:36076',
+        'sign-in' => 'http://47.98.193.2:36081',
+        'job' => 'http://47.98.193.2:36011',
+        'questionnaireBase' => 'http://47.98.193.2:36121',
+        'organization' => 'http://47.98.193.2:36106',
         'gateway' => 'https://api.cloud.wozhipei.com',
     ];
 
